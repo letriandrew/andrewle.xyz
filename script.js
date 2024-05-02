65cb1b4724c30f6b89a0944b16e52f5e2c7a6a2e
Andrew Le <ale@Andrews-MacBook-Pro.local>
2024-05-01 20:30:06 -0700

    mobile mode but hamburger menu broken

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-// The scroll effect for "Andrew Le"
+// The scroll effect for "Andrew Le" for regular size
 ScrollTrigger.create({
     animation: gsap.from(".logo", {
         y: "50vh", 
@@ -14,31 +14,49 @@ ScrollTrigger.create({
     end: "top center"
 });
 
+// The scroll effect for "Andrew Le" for mobile
+ScrollTrigger.create({
+    animation: gsap.from(".logo2", {
+        y: "50vh", 
+        scale: 3,
+        yPercent: -50
+    }),
+    scrub: true,
+    trigger: ".content",
+    start: "top bottom",
+    endTrigger: ".content",
+    end: "top center"
+});
+
 // The rolling effect for "Andrew Le"
-const h1Element = document.querySelector("h1");
+const h1Elements = document.querySelectorAll(".logo, .logo2");
 let isRollingEnabled = true;
 
 const rollOnce = () => {
     if (isRollingEnabled) {
         let iterations = 0;
         const interval = setInterval(() => {
-            h1Element.innerText = h1Element.innerText.split("")
-                .map((letter, index) => {
-                    if (index < iterations) {
-                        return h1Element.dataset.value[index];
-                    }
-                    return letters[Math.floor(Math.random() * 26)];
-                })
-                .join("");
+            h1Elements.forEach((h1Element) => {
+                h1Element.innerText = h1Element.innerText.split("")
+                    .map((letter, index) => {
+                        if (index < iterations) {
+                            return h1Element.dataset.value[index];
+                        }
+                        return letters[Math.floor(Math.random() * 26)];
+                    })
+                    .join("");
+            });
 
-            if (iterations >= h1Element.dataset.value.length) {
+            if (iterations >= h1Elements[0].dataset.value.length) {
                 clearInterval(interval);
                 isRollingEnabled = false; // Disable rolling after one iteration
             }
 
-            iterations += 1/8;
-        }, 40);
+            iterations += 1/5;
+        }, 30);
     }
 };
 
-h1Element.addEventListener("mouseover", rollOnce);
+h1Elements.forEach((h1Element) => {
+    h1Element.addEventListener("mouseover", rollOnce);
+});
